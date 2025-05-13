42a3d1ff5e01055ff594a4ce629e79d8d1d9dd33
alfazachmmosi@TimeMachine <zalfa3890@gmail.com>
2025-05-13 08:06:53 +0300

    do lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = []
+    for i in range(length):
+        if i == 0:
+            sequence.append(0)
+        elif i == 1:
+            sequence.append(1)
+        else:
+            sequence.append(sequence[i - 1] + sequence[i - 2])
+    print(sequence)
+print_fibonacci(9) 
+         
